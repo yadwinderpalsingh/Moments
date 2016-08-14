45f574479890dfc4ace4fcdfb359b95cebd677f4
yadwinderpalsingh <yadwinderpalsingh.1@gmail.com>
2016-08-14 19:09:38 -0400

    slide show added, camera enabled

--- Moments/MomentViewController.swift
+++ Moments/MomentViewController.swift
@@ -14,30 +14,76 @@ class MomentViewController: UIViewController {
     var assetCollection: PHAssetCollection!
     var photosAsset: PHFetchResult!
     var index: Int = 0
-
-    @IBAction func btnCancel(sender: AnyObject) {
-        print("Cancel")
-        self.navigationController?.popToRootViewControllerAnimated(true)
-    }
     
     @IBAction func btnExport(sender: AnyObject) {
         print("Export")
     }
     
     @IBAction func btnTrash(sender: AnyObject) {
-        print("Trash")
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .ActionSheet)
+        alert.addAction(UIAlertAction(title: "Delete Photo", style: .Default,
+            handler: {(alertAction)in
+                PHPhotoLibrary.sharedPhotoLibrary().performChanges({
+                    //Delete Photo
+                    if let request = PHAssetCollectionChangeRequest(forAssetCollection: self.assetCollection){
+                        request.removeAssets([self.photosAsset[self.index]])                    }
+                    },
+                    completionHandler: {(success, error)in
+                        alert.dismissViewControllerAnimated(true, completion: nil)
+                        
+                        if(success){
+                            // Move to the main thread to execute
+                            dispatch_async(dispatch_get_main_queue(), {
+                                self.photosAsset = PHAsset.fetchAssetsInAssetCollection(self.assetCollection, options: nil)
+                                if(self.photosAsset.count == 0){
+                                    self.navigationController?.popToRootViewControllerAnimated(true)
+                                }else{
+                                    if(self.index >= self.photosAsset.count){
+                                        self.index = self.photosAsset.count - 1
+                                    }
+                                    self.displayMoment()
+                                }
+                            })
+                        }
+                    }
+                )
+            })
+        )
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: {(alertAction)in
+            alert.dismissViewControllerAnimated(true, completion: nil)
+        }))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
     }
     
     @IBOutlet weak var moment: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(MomentViewController.swiped(_:))) // put : at the end of method name
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(MomentViewController.swiped(_:))) // put : at the end of method name
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.Left
+        self.view.addGestureRecognizer(swipeLeft)
     }
     
     override func viewWillAppear(animated: Bool) {
         self.navigationController?.hidesBarsOnTap = true
         
         self.displayMoment()
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return navigationController?.navigationBarHidden == true
+    }
+    
+    override func preferredStatusBarUpdateAnimation() -> UIStatusBarAnimation {
+        // Select a type of animation (.Fade, .Slide or .None)
+        return UIStatusBarAnimation.Slide
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,9 +93,47 @@ class MomentViewController: UIViewController {
     
     func displayMoment() {
         let imageManager = PHImageManager.defaultManager()
-        imageManager.requestImageForAsset(self.photosAsset[index] as! PHAsset, targetSize: PHImageManagerMaximumSize, contentMode: .AspectFit , options: nil, resultHandler: {(result:UIImage?, info: [NSObject: AnyObject]?) -> Void in
-                self.moment.image = result
-        })
+        imageManager.requestImageForAsset(self.photosAsset[index] as! PHAsset, targetSize: PHImageManagerMaximumSize, contentMode: .AspectFit , options: PHImageRequestOptions(), resultHandler: {(result:UIImage?, info: [NSObject: AnyObject]?) -> Void in
+           
+            UIView.transitionWithView(self.moment,
+                duration:0.5,
+                options: UIViewAnimationOptions.CurveEaseOut,
+                animations: { self.moment.image = result },
+                completion: nil)
+            }
+        )
+        
     }
-
+    
+    func swiped(gesture: UIGestureRecognizer) {
+        
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            
+            switch swipeGesture.direction {
+                
+            case UISwipeGestureRecognizerDirection.Right :
+                index -= 1
+                
+                if index < 0 {
+                    index = self.photosAsset.count - 1
+                }
+                
+                self.moment.image = nil
+                self.displayMoment()
+                
+            case UISwipeGestureRecognizerDirection.Left:
+                index += 1
+                
+                if index > self.photosAsset.count - 1 {
+                    index = 0
+                }
+                
+                self.moment.image = nil
+                self.displayMoment()
+                
+            default:
+                break
+            }
+        }
+    }
 }
--- Moments/ViewController.swift
+++ Moments/ViewController.swift
@@ -12,7 +12,7 @@ import Photos
 let reuseIdentifier = "PhotoCell"
 let albumName = "My Moments"
 
-class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     var albumFound: Bool = false
     var assetCollection: PHAssetCollection!
@@ -22,11 +22,30 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
     
     @IBAction func btnCamera(sender: AnyObject) {
-        
+        if(UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)){
+            //load the camera interface
+            let picker : UIImagePickerController = UIImagePickerController()
+            picker.sourceType = UIImagePickerControllerSourceType.Camera
+            picker.delegate = self
+            picker.allowsEditing = false
+            self.presentViewController(picker, animated: true, completion: nil)
+        }else{
+            //no camera available
+            let alert = UIAlertController(title: "Error", message: "There is no camera available", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "Okay", style: .Default, handler: {(alertAction)in
+                alert.dismissViewControllerAnimated(true, completion: nil)
+            }))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
     }
     
     @IBAction func btnPhotoLibrary(sender: AnyObject) {
-        
+        let picker : UIImagePickerController = UIImagePickerController()
+        picker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        picker.mediaTypes = UIImagePickerController.availableMediaTypesForSourceType(.PhotoLibrary)!
+        picker.delegate = self
+        picker.allowsEditing = false
+        self.presentViewController(picker, animated: true, completion: nil)
     }
     
     @IBOutlet weak var momentCollection: UICollectionView!
@@ -34,6 +53,8 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.navigationController?.hidesBarsOnTap = false
         
         // Check if the folder exists, if not, create it
         
@@ -49,22 +70,33 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
             self.getAssetCollection()
         }
         else{
+            //Album placeholder for the asset collection, used to reference collection in completion handler
+            var albumPlaceholder: PHObjectPlaceholder!
+            
             // create the folder
-            NSLog("\nFolder \"%@\" does not exist\nCreating now...", albumName)
             PHPhotoLibrary.sharedPhotoLibrary().performChanges({
-                    PHAssetCollectionChangeRequest.creationRequestForAssetCollectionWithTitle(albumName)
+                let request = PHAssetCollectionChangeRequest.creationRequestForAssetCollectionWithTitle(albumName)
+                    albumPlaceholder = request.placeholderForCreatedAssetCollection
                 },
                 completionHandler: {(success:Bool, error:NSError?)in
-                    NSLog("Creation of folder -> %@", (success ? "Success":"Error!"))
-                    self.albumFound = (success ? true:false)
-                    self.getAssetCollection()
-                })
+                    if(success){
+                        print("Successfully created folder")
+                        self.albumFound = true
+                        let collection = PHAssetCollection.fetchAssetCollectionsWithLocalIdentifiers([albumPlaceholder.localIdentifier], options: nil)
+                        self.assetCollection = collection.firstObject as! PHAssetCollection
+                    }
+                    else{
+                        print("Error creating folder")
+                        self.albumFound = false
+                    }
+            })
         }
     }
     
     // fetch the photos from collection
     
     func getAssetCollection() {
+        
         self.photosAsset = PHAsset.fetchAssetsInAssetCollection(self.assetCollection, options: nil)
         
         self.momentCollection.reloadData()
@@ -77,11 +109,15 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier! as String == "viewMomentThumbnail" {
-            let controller: MomentViewController = segue.destinationViewController as! MomentViewController
-            let indexPath: NSIndexPath = self.momentCollection.indexPathForCell(sender as! UICollectionViewCell)!
-            controller.index = indexPath.item
-            controller.photosAsset = self.photosAsset
-            controller.assetCollection = self.assetCollection
+            if let controller:MomentViewController = segue.destinationViewController as? MomentViewController{
+                if let cell = sender as? UICollectionViewCell{
+                    if let indexPath: NSIndexPath = self.momentCollection.indexPathForCell(cell){
+                        controller.index = indexPath.item
+                        controller.photosAsset = self.photosAsset
+                        controller.assetCollection = self.assetCollection
+                    }
+                }
+            }
         }
     }
 
@@ -116,6 +152,37 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat{
         return 1
+    }
+    
+    // UIImagePickerControllerDelegate Methods
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image: UIImage = info["UIImagePickerControllerOriginalImage"] as? UIImage{
+            
+            //Implement if allowing user to edit the selected image
+            //let editedImage = info.objectForKey("UIImagePickerControllerEditedImage") as UIImage
+            
+            let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
+            dispatch_async(dispatch_get_global_queue(priority, 0), {
+                PHPhotoLibrary.sharedPhotoLibrary().performChanges({
+                    let createAssetRequest = PHAssetChangeRequest.creationRequestForAssetFromImage(image)
+                    let assetPlaceholder = createAssetRequest.placeholderForCreatedAsset
+                    if let albumChangeRequest = PHAssetCollectionChangeRequest(forAssetCollection: self.assetCollection, assets: self.photosAsset) {
+                        albumChangeRequest.addAssets([assetPlaceholder!])
+                    }
+                    }, completionHandler: {(success, error)in
+                        dispatch_async(dispatch_get_main_queue(), {
+                            NSLog("Adding Image to Library -> %@", (success ? "Sucess":"Error!"))
+                            picker.dismissViewControllerAnimated(true, completion: nil)
+                        })
+                })
+                
+            })
+        }
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController){
+        picker.dismissViewControllerAnimated(true, completion: nil)
     }
 
 }
